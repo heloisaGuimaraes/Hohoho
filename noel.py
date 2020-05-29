405c9dfc0f03f0caa7e04c1afa45169b88f6e1b0
Heloisa <heloisaguimaraes86@gmail.com>
2020-05-29 18:37:51 -0300

    Final version of the project

---
+++ noel.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+@author: toti.cavalcanti
+"""
+def solution(num):
+    return "Ho " * (num - 1) + "Ho!" 
---
+++ test_noel.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: toti.cavalcanti
+"""
+import unittest
+from noel import solution
+
+class TestNoel(unittest.TestCase):
+    def test_one_Ho(self):
+        self.assertEqual(solution(1), "Ho!")
+
+    def test_two_Ho(self):
+        self.assertEqual(solution(2), "Ho Ho!")
+    
+    def test_five_Ho(self):
+        self.assertEqual(solution(5), "Ho Ho Ho Ho Ho!")
+
+unittest.main()
